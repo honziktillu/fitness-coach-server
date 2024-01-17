35ff7abe479d31c5508b8f1094cff63c9ab0d9ab
honziktillu <till@spsmb.cz>
2024-01-17 14:41:59 +0100

    init

---
+++ src/controllers/user.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+
+export const getAllUsers = (req: Request, res: Response) => {
+  res.send("Never go full Vojta");
+};
+export const getUserById = (req: Request, res: Response) => {
+  res.send("Never go full Vojta");
+};
+
+export const createUser = (req: Request, res: Response) => {
+  res.send("Never go full Vojta");
+};
+
+export const updateUser = (req: Request, res: Response) => {
+  res.send("Never go full Vojta");
+};
+
+export const deleteUser = (req: Request, res: Response) => {
+  res.send("Never go full Vojta");
+};
---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config(); // načte .env soubor do process.env
+//process.env.PORT
+import cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+
+app.use(`/api/v${process.env.API_VER}/user`, require("./routes/user"));
+
+app.listen(PORT, () => {
+  console.log(`App is running on ${PORT}`);
+});
